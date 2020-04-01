8eaf1d5b1dc6f2687957cf18c748359ef7270662
Artiya Thinkumpang <artiya4u@gmail.com>
2020-04-02 00:23:37 +0700

    Remove unused code

--- app/src/main/java/com/bootlegsoft/wellmet/App.java
+++ app/src/main/java/com/bootlegsoft/wellmet/App.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 public class App extends Application {
 
     private static final Logger sLogger = LoggerFactory.getLogger(App.class);
@@ -39,29 +38,6 @@ public class App extends Application {
 
     public void startResilientBeacons() {
         IBeaconSimulatorService.startBroadcast(this);
-    }
-
-    public void enableRebootResilience(boolean enable) {
-        final int componentState = mPm.getComponentEnabledSetting(mRebootReceiverComponent);
-        boolean lastState = false;
-        switch (componentState) {
-            case PackageManager.COMPONENT_ENABLED_STATE_ENABLED:
-                lastState = true;
-                break;
-            case PackageManager.COMPONENT_ENABLED_STATE_DEFAULT: // Shoud be explicitely disabled by default in the manifest
-            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED:
-                lastState = false;
-                break;
-            default:
-        }
-        if (lastState == enable) {
-            return;
-        }
-        final int status = enable
-                ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-        mPm.setComponentEnabledSetting(mRebootReceiverComponent, status, PackageManager.DONT_KILL_APP);
-        sLogger.info("Setting broadcast resilience to: {}", enable);
     }
 
 
