b95c613b7adacc3c1992f4e271b1aca27de031f1
Artiya Thinkumpang <artiya4u@gmail.com>
2020-04-04 17:32:08 +0700

    Add dashboard UI.

--- app/src/main/java/com/bootlegsoft/wellmet/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/bootlegsoft/wellmet/ui/dashboard/DashboardFragment.java
@@ -7,9 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.bootlegsoft.wellmet.R;
@@ -23,13 +21,7 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final TextView textView = root.findViewById(R.id.level);
         return root;
     }
 }
