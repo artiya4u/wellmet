eb91432c75a9fad1d0b7e2df84c49d8297e40bcb
Artiya Thinkumpang <artiya4u@gmail.com>
2020-04-11 19:17:39 +0700

    Change number to data in the contact tracing spec.

--- app/src/main/java/com/bootlegsoft/wellmet/Utils.java
+++ app/src/main/java/com/bootlegsoft/wellmet/Utils.java
@@ -2,6 +2,7 @@ package com.bootlegsoft.wellmet;
 
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -36,10 +37,12 @@ public class Utils {
         return b;
     }
 
-    public static byte[] longToBytes(long x) {
-        ByteBuffer buffer = ByteBuffer.allocate(8);
-        buffer.putLong(x);
-        return buffer.array();
+    public static byte[] longToUnsignedInt(long x) {
+        int newInt = (int) x;
+        ByteBuffer bb = ByteBuffer.allocate(4);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.putInt(newInt);
+        return bb.array();
     }
 
     public static String genTracingKey(String salt) {
@@ -62,7 +65,7 @@ public class Utils {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(tracingKey);
             md.update(CT_DTK.getBytes(StandardCharsets.UTF_8));
-            md.update(longToBytes(dayNumber));
+            md.update(longToUnsignedInt(dayNumber));
             byte[] digest = md.digest();
             // Truncate to 16 bytes
             ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
@@ -80,7 +83,7 @@ public class Utils {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(dailyTracingKey);
             md.update(CT_RPI.getBytes(StandardCharsets.UTF_8));
-            md.update(longToBytes(timeIntervalNumber));
+            md.update((byte) (timeIntervalNumber));
             byte[] digest = md.digest();
             // Truncate to UUID
             return UUID.nameUUIDFromBytes(digest);
